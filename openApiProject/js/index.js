b65422fc0ef1eeeb429bee2daaa73cb9bf53f9ef
altwake <145942324+altwake@users.noreply.github.com>
2024-05-28 11:22:07 -0400

    API fetch completed

--- openApiProject/js/index.js
+++ openApiProject/js/index.js
@@ -1,16 +1,49 @@
-fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
+const apiKey = "ee4a5b05be34c53f5e05502708aee5ce";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button"); 
+const weatherIcon = document.querySelector(".weather-icon");
 
-    .then(data => {
+async function checkWeather(city) {
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        const data = await response.json();
         console.log(data);
-    })
 
-    .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-    });
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        switch (data.weather[0].main) {
+            case "Clouds":
+                weatherIcon.src = "images/clouds.jpeg";
+                break;
+            case "Clear":
+                weatherIcon.src = "images/clear.jpeg";
+                break;
+            case "Rain":
+                weatherIcon.src = "images/rain.jpeg";
+                break;
+            case "Mist":
+                weatherIcon.src = "images/mist.jpeg";
+                break;
+            default:
+                weatherIcon.src = "images/weather.jpeg";
+        }
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    const city = searchBox.value.trim();
+    if (city) {
+        checkWeather(city);
+    }
+});
